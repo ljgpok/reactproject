8c6af0c2d8433aee0a5fd8733bdfea58218c4311
rebel0709 <snakeInShade@163.com>
2018-04-06 04:38:54 +0800

    Update auth_reducer.js
    second commit

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -10,7 +10,7 @@ export default function(state = {}, action) {
     case AUTH_USER:
       return { ...state, error: '', authenticated: true };
     case UNAUTH_USER:
-      return { ...state, authenticated: false };
+      return { ...state, authenticated: false , message:null};
     case AUTH_ERROR:
       return { ...state, error: action.payload };
     case FETCH_MESSAGE:
